6cba46463d5f61232be47e1bbab57a1cf2e4826d
Mike Innes <mike.j.innes@gmail.com>
2015-10-12 18:41:14 +0100

    cleanup

--- src/bench.jl
+++ src/bench.jl
@@ -5,9 +5,6 @@ data = DataSet(data)
 
 splitrange(n, p = 0.7) = 1:round(Int, n*p), round(Int, n*p)+1:n
 
-int(150*0.7)
-int(150*(1-0.7))
-
 function class_act(data, y)
   trainrange, testrange = splitrange(length(data))
   train, test = data[trainrange], data[testrange]
