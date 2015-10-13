f8221a7e4e7e82e949401866ac63d3a0b5f9281c
Mike Innes <mike.j.innes@gmail.com>
2015-10-13 15:00:59 +0100

    nullable support

--- src/nulls.jl
+++ src/nulls.jl
@@ -2,4 +2,7 @@ import Data: vartype
 
 vartype{T}(::Type{Nullable{T}}) = vartype(T)
 
-isleft(::Categorical, x::Nullable, y::Nullable) = isequal(x, y)
+isleft(::Categorical, x::Nullable, z::Nullable) = isequal(x, z)
+
+isleft(::Continuous, x::Nullable, z::Nullable) =
+  isequal(x, z) || (!(isnull(x) || isnull(z)) && get(x) ≤ get(z))
