1bb540e87514f4b60f3ffacf6538cff90e0230f1
Mike Innes <mike.j.innes@gmail.com>
2015-10-13 12:51:08 +0100

    add expression simplification

--- src/meta.jl
+++ src/meta.jl
@@ -1,8 +1,49 @@
+using Lazy
+import MacroTools: postwalk
+
+isleftexpr(x, z) = isleftexpr(vartype(z), x, z)
+
+isleftexpr(::Categorical, x, z) = :($x == $z)
+isleftexpr(::Continuous, x, z) = :($x ≤ $z)
+
 function expr(b::Branch)
   isleaf(b) && return b.val
-  :(if isleft(data[Field{$(Expr(:quote, b.col))}()], $(b.val))
+  :(if isleft($(b.col), $(b.val))
       $(expr(get(b.left)))
     else
       $(expr(get(b.right)))
     end)
+end
+
+function simplify_if(ex)
+  postwalk(ex) do ex
+    @match ex begin
+      (c_ ? t_ : t_) => t
+      (c_ ? (c′_ ? t_ : f_) : f_) => :($c && $c′ ? $t : $f)
+      (c_ ? (c′_ ? f_ : t_) : f_) => :($c && !$c′ ? $t : $f)
+      (c_ ? t_ : (c′_ ? t_ : f_)) => :($c && $c′ ? $t : $f)
+      (c_ ? t_ : (c′_ ? f_ : t_)) => :($c && !$c′ ? $t : $f)
+      x_ => x
+    end
+  end
+end
+
+function simplify_cond(ex)
+  postwalk(ex) do ex
+    @match ex begin
+      !(a_ && b_) => :($a || $b)
+      x_ => x
+    end
+  end
+end
+
+simplify(ex) = @> ex simplify_if simplify_cond
+
+function humanify(ex)
+  postwalk(simplify(ex)) do ex
+    @match ex begin
+      isleft(args__) => isleftexpr(args...)
+      x_ => x
+    end
+  end
 end
