e14d858e180ddb2644c40e3125d88a3338790e1e
Mike Innes <mike.j.innes@gmail.com>
2015-10-14 20:40:18 +0100

    pooled data vectors

--- src/integrations.jl
+++ src/integrations.jl
@@ -1,3 +1,5 @@
+# Nullable Arrays
+
 import Data: vartype
 
 vartype{T}(::Type{Nullable{T}}) = vartype(T)
@@ -6,3 +8,13 @@ isleft(::Categorical, x::Nullable, z::Nullable) = isequal(x, z)
 
 isleft(::Continuous, x::Nullable, z::Nullable) =
   isequal(x, z) || (!(isnull(x) || isnull(z)) && get(x) ≤ get(z))
+
+# Pooled Data
+
+function pcat(xs::PooledVector)
+  counts = zeros(length(xs.values))
+  for x in xs.data
+    @inbounds counts[x] += 1
+  end
+  return scale!(counts, 1/length(xs))
+end
