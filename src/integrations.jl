4f147214d52e775b60c1fec26c47cd86bd0628ce
Mike Innes <mike.j.innes@gmail.com>
2015-10-15 13:44:06 +0100

    generalise to pooled views

--- src/integrations.jl
+++ src/integrations.jl
@@ -7,33 +7,39 @@ isleft(::Continuous, x::Nullable, z::Nullable) =
 
 # Pooled Data
 
-function pcat(xs::PooledVector)
-  counts = zeros(length(xs.values))
-  for x in xs.data
+import Data: getdata
+
+typealias Pooled Union{PooledVector, SubPooledVector}
+
+function pcat(xs::Pooled)
+  counts = zeros(length(names(xs)))
+  for x in getdata(xs)
     @inbounds counts[x] += 1
   end
   return scale!(counts, 1/length(xs))
 end
 
-function score_inner!(xs, ys::PooledVector, z, left, right)
+function score_inner!(xs, ys_::Pooled, z, left, right)
+  ys = getdata(ys_)
   for i = 1:length(xs)
-    @inbounds (isleft(xs[i], z) ? left : right)[ys.data[i]] += 1
+    @inbounds (isleft(xs[i], z) ? left : right)[ys[i]] += 1
   end
 end
 
-function score_inner!(xs::PooledVector, ys::PooledVector, z, left, right)
-  nx = length(unique(xs))
+function score_inner!(xs::Pooled, ys::Pooled, z, left, right)
+  xs_, ys_ = getdata(xs), getdata(ys)
+  nx = length(names(xs))
   lefts = Array(Bool, nx)
   for i = 1:nx
-    lefts[i] = isleft(unique(xs)[i], z)
+    lefts[i] = isleft(names(xs)[i], z)
   end
   for i = 1:length(xs)
-    @inbounds (lefts[xs.data[i]] ? left : right)[ys.data[i]] += 1
+    @inbounds (lefts[xs_[i]] ? left : right)[ys_[i]] += 1
   end
 end
 
-function score(xs, ys::PooledVector, z)
-  left, right = zeros(length(unique(ys))), zeros(length(unique(ys)))
+function score(xs, ys::Pooled, z)
+  left, right = zeros(length(names(ys))), zeros(length(names(ys)))
   score_inner!(xs, ys, z, left, right)
   n, nleft, nright = length(ys), sum(left), sum(right)
   pleft, pright = nleft/n, nright/n
