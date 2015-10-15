9e1cac3de0862678d87b7f0ae170b6033ed4096d
Mike Innes <mike.j.innes@gmail.com>
2015-10-15 21:55:13 +0100

    remove subpooledarrays

--- src/integrations.jl
+++ src/integrations.jl
@@ -7,38 +7,31 @@ isleft(::Continuous, x::Nullable, z::Nullable) =
 
 # Pooled Data
 
-import Data: getdata
-
-typealias Pooled Union{PooledVector, SubPooledVector}
-
-function pcat(xs::Pooled)
+function pcat(xs::PooledVector)
   counts = zeros(length(names(xs)))
-  for x in getdata(xs)
+  for x in xs.data
     @inbounds counts[x] += 1
   end
   return scale!(counts, 1/length(xs))
 end
 
-function score_inner!(xs, ys_::Pooled, z, left, right)
-  ys = getdata(ys_)
+function score_inner!(xs, ys::PooledVector, z, left, right)
   for i = 1:length(xs)
-    @inbounds (isleft(xs[i], z) ? left : right)[ys[i]] += 1
+    @inbounds (isleft(xs[i], z) ? left : right)[ys.data[i]] += 1
   end
 end
 
-function score_inner!(xs::Pooled, ys::Pooled, z, left, right)
-  xs_, ys_ = getdata(xs), getdata(ys)
-  nx = length(names(xs))
-  lefts = Array(Bool, nx)
-  for i = 1:nx
+function score_inner!(xs::PooledVector, ys::PooledVector, z, left, right)
+  lefts = Array(Bool, length(names(xs)))
+  for i = 1:length(names(xs))
     lefts[i] = isleft(names(xs)[i], z)
   end
   for i = 1:length(xs)
-    @inbounds (lefts[xs_[i]] ? left : right)[ys_[i]] += 1
+    @inbounds (lefts[xs.data[i]] ? left : right)[ys.data[i]] += 1
   end
 end
 
-function score(xs, ys::Pooled, z;
+function score(xs, ys::PooledVector, z;
                orig = gini(pcat(ys)))
   left, right = zeros(length(names(ys))), zeros(length(names(ys)))
   score_inner!(xs, ys, z, left, right)
