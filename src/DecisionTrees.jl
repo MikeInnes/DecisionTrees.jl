3bd099d90110a1e681563f14f114ae45eaf47523
Mike Innes <mike.j.innes@gmail.com>
2015-10-17 15:20:55 +0100

    tree.jl -> training.jl

--- src/DecisionTrees.jl
+++ src/DecisionTrees.jl
@@ -3,7 +3,7 @@ module DecisionTrees
 using MacroTools, Data, Lazy, StatsBase
 
 include("impurity.jl")
-include("tree.jl")
+include("training.jl")
 include("integrations.jl")
 include("meta.jl")
 include("bench.jl")
